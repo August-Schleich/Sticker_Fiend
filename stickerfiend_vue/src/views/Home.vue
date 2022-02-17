29cade6ce1dd64e1b5465eea35b0d6eafca37838
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-02-17 10:50:18 -0800

    Created: View for Poducts: django views, vue page for showing, detail link

--- stickerfiend_vue/src/views/Home.vue
+++ stickerfiend_vue/src/views/Home.vue
@@ -23,13 +23,13 @@
       >
       
         <div class="box">
-          <figure class="image mb-4">
+          <figure class="image mb-4 ">
             <img :src="product.get_thumbnail">
             </figure>
 
               <h3 class ="is-size-4">{{ product.name }}</h3>
               <p class ="is-size-6 has-text-grey">${{ product.price }}</p>
-              View Details
+               <router-link v-bind:to="product.get_absolute_url" class = "button is-dark mt-4">View Details</router-link>
           </div>
       </div>
     </div>
