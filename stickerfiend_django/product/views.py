939468cd75b0b3014abdbcefbd44767a51e15510
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-03-01 13:28:43 -0800

    Added: ability to veiw category products: category serializer,view,urls and Category.vue

--- stickerfiend_django/product/views.py
+++ stickerfiend_django/product/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.views import Response
 
 from .models import Product, Category
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, CategorySerializer
 
 
 class LatestProductList(APIView):
@@ -19,9 +19,22 @@ class ProductDetail(APIView):
         try:
             return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
         except Product.DoesNotExist:
-            raise 
-    
+            raise Http404
+        
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+    
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
