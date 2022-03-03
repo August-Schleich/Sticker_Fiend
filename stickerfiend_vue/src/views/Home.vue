8ef07386c8659c477c2b8303fde1b242bfb77406
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-03-02 16:54:54 -0800

    finished Category.vue views and added watch to router and made product into component

--- stickerfiend_vue/src/views/Home.vue
+++ stickerfiend_vue/src/views/Home.vue
@@ -16,29 +16,20 @@
       <h2 class="is-size-2 has-text-centered">Latest Stickers</h2>
     </div>
      
-    <div 
-      class ="column is-3" 
-      v-for="product in latestProducts" 
-      v-bind:key="product.id"
-      >
-      
-        <div class="box">
-          <figure class="image mb-4 is-4">
-            <img :src="product.get_thumbnail">
-            </figure>
-
-              <h3 class ="is-size-4">{{ product.name }}</h3>
-              <p class ="is-size-6 has-text-grey">${{ product.price }}</p>
-               <router-link v-bind:to="product.get_absolute_url" class = "button is-dark mt-4">View Details</router-link>
-          </div>
+      <ProductBox
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
       </div>
     </div>
-  </div>
+  
 
 </template>
 
 <script>
 import axios from 'axios'
+ 
+import ProductBox from '@/components/ProductBox'
 
 export default {
   name: 'Home',
@@ -47,7 +38,8 @@ export default {
       latestProducts: []
     }
   },
-  components: {
+  components: { 
+    ProductBox,
   },
   mounted(){
     this.getLatestProducts()
@@ -67,11 +59,4 @@ export default {
   }
 }
 </script>
-<style scoped>
-.image {
-  margin-top: -1.25rem;
-  margin-left: -1.25rem;
-  margin-right: -1.25rem;
 
-}
-</style>
