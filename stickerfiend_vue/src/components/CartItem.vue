ee9fbb744eebc601991fc9c3bf8b8586ec5866cd
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-03-10 14:50:41 -0800

    Added: change content of Cart products +, - and remove

--- stickerfiend_vue/src/components/CartItem.vue
+++ stickerfiend_vue/src/components/CartItem.vue
@@ -4,8 +4,8 @@
         <td>${{ item.product.price }}</td>
         <td>
             {{ item.quantity }}
-            <a @click="decrementQuantity(item)">-</a>
-            <a @click="incrementQuantity(item)">+</a>
+            <a @click="decrementQuantity(item)"> - </a>
+            <a @click="incrementQuantity(item)"> + </a>
         </td>
         <td>${{ getItemTotal(item).toFixed(2) }}</td>
         <td><button class="delete" @click="removeFromCart(item)"></button></td>
@@ -27,6 +27,28 @@ export default {
          getItemTotal(item) {
              return item.quantity * item.product.price
          },
-     },
+         decrementQuantity(item){
+               item.quantity -= 1
+
+                this.updateCart()
+                if (item.quantity === 0) {
+                    this.$emit('removeFromCart',item)
+                }
+
+         },
+         incrementQuantity(item){
+             item.quantity += 1
+             this.updateCart()
+         },
+         updateCart(){
+             localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
+         },
+         removeFromCart(item){
+             this.$emit('removeFromCart', item)
+
+             this.updateCart()
+
+         }
+     }, 
 }
 </script>
