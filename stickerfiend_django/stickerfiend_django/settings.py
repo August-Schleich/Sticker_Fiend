e905ab133bf3501de80500931b40546c1e5d2cba
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-03-28 16:59:39 -0700

    Added simple account page, Fixed django rest auth in settings

--- stickerfiend_django/stickerfiend_django/settings.py
+++ stickerfiend_django/stickerfiend_django/settings.py
@@ -49,14 +49,14 @@ INSTALLED_APPS = [
 ]
 
 #configure DRF
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.TokenAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
 
 
 
--- stickerfiend_vue/src/router/index.js
+++ stickerfiend_vue/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import store from '../store'
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
@@ -7,6 +7,7 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 const routes = [
   {
@@ -48,6 +49,14 @@ const routes = [
     component: SignUp
   },
   {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: { 
+      requireLogin: true
+    }
+  },
+  {
     path: '/log-in',
     name: 'LogIn',
     component: LogIn
@@ -57,5 +66,17 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
+})
+
+
+
 
 export default router
